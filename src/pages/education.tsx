e56915f7f7efae8c128403767d099dd1ee5a34ee
Anindra123 <bivasdas911@gmail.com>
2023-04-11 13:30:32 +0600

    Added routes

---
+++ src/pages/education.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent } from "react";
+import Link from 'next/link'
+
+
+const Education: FunctionComponent = () => {
+
+    return (
+
+        <div className='flex flex-col items-center justify-center bg-slate-800 text-center w-screen h-screen'>
+            <p className='text-3xl'> Might have a degree 👨‍🎓</p>
+
+            <Link className='w-1/5 m-2 h-max pb-2 pt-2 hover:bg-slate-500 rounded-lg text-2xl bg-slate-600 ' href='/'>Back To Home 🏠</Link>
+        </div>
+
+    )
+
+}
+
+export default Education 
