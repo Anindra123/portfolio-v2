ff3ed2eb4be23d129d4a2ae3219cf02f277d619a
Anindra123 <bivasdas911@gmail.com>
2023-04-13 23:24:22 +0600

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,24 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-
 export default function Home() {
   return (
-    <div className=' bg-gray-800 w-screen h-screen'>
+    <div className=' bg-zinc-600 w-screen h-screen'>
       <div className='flex flex-row w-screen items-center h-10 border-b-2  border-violet-400'>
         <p className='flex-1 text-2xl text-violet-400'>adb.</p>
-        <nav className='flex-1 text-violet-400 '>
+        <nav className='flex-2 text-violet-400 '>
           <Link href='/skills' >Skills</Link>
           <Link href='/skills' >Skills</Link>
           <Link href='/skills' >Skills</Link>
           <Link href='/skills' >Skills</Link>
         </nav>
 
-        <div>
-          <Image src='./public/my_pic.jgp' width={50} height={50}
 
-            alt='my profile picture' />
+      </div>
+      <div>
+        <Image src='/images/my_pic.jpg' width={50} height={50}
 
-        </div>
+          alt='my profile picture' />
 
       </div>
     </div>
