ffe5122d0d748e37301b68171b25eb7695acbc05
Anindra123 <bivasdas911@gmail.com>
2023-04-14 17:34:25 +0600

    updated image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,18 @@ import Layout from '@/components/layout'
 export default function Home() {
   return (
     <Layout page_title='ADB Porfolio Home'>
-      <div className='flex flex-col w-screen h-3/4 bg-indigo-500 items-center'>
+      <div className='flex flex-col w-screen h-3/4  items-center'>
 
-        <div className=' w-5/6 flex flex-col items-center  bg-amber-300   p-4'>
-          <Image src='/images/my_pic.jpg' className=' rounded-full'
+        <div className=' w-5/6 flex flex-col items-center  bg-amber-300 m-10 '>
+          <Image src='/images/my_pic.png' className='rounded-full outline-offset-2 border-spacing-4 outline-dashed outline-violet-400 outline-dashed'
 
-            width={20} height={20}
+            width={200} height={200}
 
             alt='my profile picture' />
 
         </div>
+
+        <p></p>
       </div>
 
     </Layout>
