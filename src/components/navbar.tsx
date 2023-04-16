80b143ca790eb4621d7899029e6c544ecc84edb7
Bivas <bivasdas911@gmail.com>
2023-04-16 23:28:26 +0600

    Adding nav button for mobile screens

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import NavButton from './navbutton'
 import NavLinks from './navlinks'
 
 import Link from 'next/link'
@@ -6,15 +7,12 @@ const linkList: string[] = ['Skills', 'Education', 'Contact', 'Projects']
 
 const NavBar = () => {
     return (
-
-
         <div className='flex flex-row w-screen items-center justify-between h-28 border-b-2  border-violet-400'>
             <p className=' text-2xl text-violet-400  ml-5  flex-1'>adb.</p>
             <nav className=' text-violet-400  flex-1'>
                 <NavLinks links={linkList} style='inline-block mr-5' />
+                <NavButton />
             </nav>
-
-
         </div>
 
     )
---
+++ src/components/navbutton.tsx
@@ -0,0 +1,16 @@
+
+
+const NavButton = () => {
+    return (
+        <div className="flex flex-row justify-end
+ md:hidden  ">
+
+            <div className="w-8 h-8 bg-amber-400 outline-1 rounded-md outline outline-lime-300 outline-violet-400 ">
+
+            </div>
+        </div>
+    )
+
+}
+
+export default NavButton
